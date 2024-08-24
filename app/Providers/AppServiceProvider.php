d0a50c890df9292a21eb9a4e259d29e5ca9204c7
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-08-24 19:39:35 -0300

    Add support for pagination in job listings
    Implemented pagination for the job listings using various pagination templates. Modified the jobs route to utilize cursor pagination for better performance with large datasets. Added pagination links in the job listings view to navigate between pages.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\AliasLoader;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
+//        Paginator::useBootstrapFive();
+
     }
 }
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -6,14 +6,18 @@
     <div class="space-y-4">
         @foreach($jobs as $job)
 
-                <a href="jobs/{{$job['id']}}" class="houver:underline block px-4 py-6 border border-gray-200 rounded-lg">
-                    <div class="font-bold text-blue-500 text-sm">{{$job->employer->name}}</div>
-                    <div>
-                        <strong>{{$job['title']}}</strong>: Ganha {{$job['salary']}} por ano
-                    </div>
-                </a>
+            <a href="jobs/{{$job['id']}}" class="houver:underline block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{$job->employer->name}}</div>
+                <div>
+                    <strong>{{$job['title']}}</strong>: Ganha {{$job['salary']}} por ano
+                </div>
+            </a>
 
         @endforeach
+
+        <div>
+            {{$jobs->links()}}
+        </div>
     </div>
 
 </x-layout>
