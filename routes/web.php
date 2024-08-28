2b5ef0779ec947116acf5d0b7bc647a58cc3b1e2
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-08-28 19:07:08 -0300

    Simplify route definitions and add contact route
    Replaced closure-based routes with Route::view for static pages to streamline the code. The contact route was moved and added using Route::view for consistency with other static pages.

--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,13 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
-
-
-Route::get('/about', function () {
-    return view('about');
-});
-
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
+Route::view('/about', 'about');
 Route::get('/jobs', [JobController::class, 'index']);
-
 Route::get('/jobs/create', [JobController::class, 'create']);
-
 Route::get('/jobs/{job}', [JobController::class, 'show']);
-
 Route::post('/jobs', [JobController::class, 'store']);
-
 Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-
 Route::patch('/jobs/{job}', [JobController::class, 'update']);
-
 Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
-Route::get('/contact', function () {
-    return view('contact');
-});
