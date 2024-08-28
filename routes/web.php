4f9891cf14acff200b7eecc2a9f5961c615d3e9f
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-08-28 19:16:28 -0300

    Simplify job routes using a resource controller
    Replaced individual job routes with a single resource route for "jobs" using the JobController resource controller. This change reduces redundancy and makes the route definitions more maintainable.

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,9 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
+
 Route::view('/contact', 'contact');
+
 Route::view('/about', 'about');
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+
+Route::resource('jobs', JobController::class);
