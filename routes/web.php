e8d1af89330a4dbd6f6377c881cff6781677efd3
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-08-31 10:28:39 -0300

    Add authentication and registration components
    Implemented Blade components for form elements, created views for registration and login, and updated layout to include authentication links. Additionally, added routes and controllers for user registration and login functions.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
@@ -10,3 +12,10 @@ Route::view('/contact', 'contact');
 Route::view('/about', 'about');
 
 Route::resource('jobs', JobController::class);
+
+Route::get('/register',[RegisterController::class, 'create']);
+Route::post('/register',[RegisterController::class, 'store']);
+
+
+Route::get('/login',[SessionController::class, 'create']);
+Route::post('/login',[SessionController::class, 'store']);
